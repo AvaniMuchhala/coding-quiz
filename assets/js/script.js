702de9d50e18eb098153e32a43d68a67fbdf9167
avanim <axm1205@case.edu>
2022-11-15 20:17:55 -0500

    local storage of initials/scores and rendered Highscores page

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,6 +36,35 @@ var questionSet =
 var index = 0;
 // Create <p> to store the result of current question
 var result = document.createElement("p");
+result.setAttribute("style","color: gray; font-style: italic");
+
+// Retrieve local storage
+var playerScoreList = JSON.parse(localStorage.getItem("highscores"));   // should be array of objects
+// If no scores stored, set playerScoreList as empty array
+if(playerScoreList === null) {
+    playerScoreList = [];
+}
+
+function renderScores() {
+    playerScoreList.sort((a, b) => b.score - a.score);
+
+    // Create ordered list of highscores
+    var highscoresList = document.createElement("ol");
+    container.appendChild(highscoresList);
+
+    // Create and append each list item (player scores)
+    for (var i = 0; i < playerScoreList.length; i++) {
+        var liEl = document.createElement("li");
+        liEl.textContent = (i+1) + ". " + playerScoreList[i].initials + ": " + playerScoreList[i].score;
+        highscoresList.appendChild(liEl);
+    }
+}
+
+function storeScores(playerScore) {
+    console.log("storeScores() called");
+    playerScoreList.push(playerScore);
+    localStorage.setItem("highscores", JSON.stringify(playerScoreList));
+}
 
 function runClock() {
     timerEl.textContent = timeLeft;
@@ -55,6 +84,50 @@ function runClock() {
     }, 1000);
 }
 
+// Function to view scoreboard
+function viewScores(event) {
+    event.preventDefault();
+    console.log("viewScores called");
+
+    var initialsInput = document.querySelector("#initials").value;
+    var score = timeLeft;
+    console.log(initialsInput + " " + score);
+    var playerScore = {
+        initials: initialsInput,
+        score: score
+    };
+    // Store current player's score
+    storeScores(playerScore);
+
+    // Remove content within container from previous page
+    container.textContent = "";
+
+    // Create and append Highscores title
+    var highscoreTitle = document.createElement("h1");
+    highscoreTitle.textContent = "Highscores";
+    container.append(highscoreTitle);
+
+    renderScores();
+
+    // Create two buttons
+    var goBack = document.createElement("button");
+    goBack.textContent = "Go Back";
+    container.append(goBack);
+    // Refresh webpage to return to starting page upon click of Go Back button
+    goBack.addEventListener("click", function() {
+        window.location.reload();
+    });
+
+    var clearScores = document.createElement("button");
+    clearScores.textContent = "Clear Highscores";
+    container.append(clearScores);
+    // Clear local storage and remove highscores from page upon click
+    clearScores.addEventListener("click", function() {
+        localStorage.clear();
+        document.querySelector("ol").textContent = "";
+    });
+}
+
 function gameOver() {
     console.log("Game over!");
 
@@ -69,9 +142,12 @@ function gameOver() {
 
     // Display form to enter user's initials within container element
     var formEl = document.querySelector("form");
-    console.log(formEl);
     container.append(formEl);
     formEl.setAttribute("style","display: block");
+
+    // When Submit button clicked, show scoreboard
+    var submitEl = document.querySelector("#submit");
+    submitEl.addEventListener("click", viewScores);
 
 }
 
@@ -83,6 +159,7 @@ function checkAnswer(event) {
     if (event.target.textContent === questionSet[index].correctAnswer) {
         console.log("correct");
         result.textContent = "Correct!";
+    // If wrong answer, deduct 10 seconds from timeLeft and update time immediately    
     } else {
         console.log("wrong");
         result.textContent = "Wrong!";
@@ -94,30 +171,31 @@ function checkAnswer(event) {
     if (index === questionSet.length-1) {
         quizDone = true;
         gameOver();
-        container.appendChild(result);
     // If not last question, increment index and call nextQuestion()
     } else {
         index++;
         nextQuestion();
-        container.appendChild(result);  // Show result from previous question on the page with next question
     }
+    // Show result from previous question on the page with next question
+    container.appendChild(result);
 }
 
 // Function displays each question-answer set
 function nextQuestion(event){
-    // timerEl.textContent = timeLeft;
+    // If first question, start timer
     if (index === 0) {
         runClock();
     }
 
     // Remove content within container from starting page / previous question
     container.textContent = "";
-    // Change alignment of flex items to left
+    // Change alignment of flex items from center to left
     container.setAttribute("style","align-items: start");
 
     // Display current question
     question = document.createElement("h2");
     question.textContent = questionSet[index].question;
+    question.setAttribute("style","font-size: 2rem; margin-bottom: 1rem");
     container.appendChild(question);
 
     // Add section for all answer choice buttons
@@ -130,15 +208,15 @@ function nextQuestion(event){
     for (var i = 0; i < questionSet[index].options.length; i++) {
         var answerChoice = document.createElement("button");
         answerChoice.textContent = questionSet[index].options[i];
+        answerChoice.setAttribute("style","font-size: 1.2rem; margin: 0 0 1rem 0.5rem; color: white; background-color: purple; padding: 0.3rem");
         answerSection.appendChild(answerChoice); 
         // Each button is clickable and triggers checkAnswer()
         answerChoice.addEventListener("click", checkAnswer);    
     }
 }
 
+
+// viewScoreEl = document.getElementById("view-scores");
+// console.log(viewScoreEl);
+// viewScoreEl.addEventListener("click",viewScores);
 startButton.addEventListener("click", nextQuestion);
-
-
-
-
-
